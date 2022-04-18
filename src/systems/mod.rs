74e8f996bb25382b6f509effc73dde1bdc862669
Rob Watson <9914552+rwatson462@users.noreply.github.com>
2022-04-18 20:43:26 +0100

    Simplify movement with Movement system

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -4,6 +4,7 @@ mod entity_render;
 mod collision;
 mod move_randomly;
 mod end_turn;
+mod movement;
 
 use crate::prelude::*;
 
@@ -11,6 +12,7 @@ use crate::prelude::*;
 pub fn build_input_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
+        .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .build()
@@ -18,6 +20,8 @@ pub fn build_input_scheduler() -> Schedule {
 
 pub fn build_player_turn_scheduler() -> Schedule {
     Schedule::builder()
+        .add_system(movement::movement_system())
+        .flush()
         .add_system(collision::collision_system())
         .flush()
         .add_system(map_render::map_render_system())
@@ -29,6 +33,9 @@ pub fn build_player_turn_scheduler() -> Schedule {
 pub fn build_monster_turn_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(move_randomly::move_randomly_system())
+        .flush()
+        .add_system(movement::movement_system())
+        .flush()
         .add_system(collision::collision_system())
         .flush()
         .add_system(map_render::map_render_system())
