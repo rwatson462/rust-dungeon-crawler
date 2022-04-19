601adc296805d0e239f9ee85a9e9236038259d2c
Rob Watson <9914552+rwatson462@users.noreply.github.com>
2022-04-19 20:12:47 +0100

    Minor refactor, press H to heal

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -16,12 +16,10 @@ pub fn combat( ecs: &mut SubWorld, commands: &mut CommandBuffer) {
             .unwrap()
             .get_component_mut::<Health>()
         {
-            println!("Health before attack: {}", health.current);
             health.current -= 1;
             if health.current < 1 {
                 commands.remove(*victim);
             }
-            println!("Health after attack: {}", health.current);
         }
         commands.remove(*message);
     })
--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -12,23 +12,30 @@ pub fn player_input(
     #[resource] turn_state: &mut TurnState
 ) {
     if let Some(key) = key {
+
+        let mut players = <(Entity, &Point)>::query()
+            .filter(component::<Player>());
+
+        let (player_entity, player_pos) = players
+            .iter(ecs)
+            .find_map(|(entity,pos)| Some((*entity, *pos)))
+            .unwrap();
+
         let delta = match key {
             VirtualKeyCode::A => Point::new(-1,0),
             VirtualKeyCode::D => Point::new(1,0),
             VirtualKeyCode::W => Point::new(0,-1),
             VirtualKeyCode::S => Point::new(0,1),
+            VirtualKeyCode::H => {
+                if let Ok(mut health) = ecs.entry_mut(player_entity).unwrap().get_component_mut::<Health>() {
+                    health.current = i32::min(health.max, health.current+1);
+                }
+                Point::zero()
+            },
             _ => Point::zero()
         };
 
-        let mut players = <(Entity, &Point)>::query()
-            .filter(component::<Player>());
-
-        let (player_entity, destination) = players
-            .iter(ecs)
-            .find_map(|(entity,pos)| Some((*entity, *pos+delta)))
-            .unwrap();
-
-        let mut did_something = false;
+        let destination = player_pos + delta;
 
         if delta.x != 0 || delta.y != 0 {
             let mut hit_something = false;
@@ -38,7 +45,6 @@ pub fn player_input(
                 .filter(|(_,pos)| **pos == destination)
                 .for_each(|(entity,_)| {
                     hit_something = true;
-                    did_something = true;
                     commands.push(((), WantsToAttack {
                         attacker: player_entity,
                         victim: *entity
@@ -49,17 +55,10 @@ pub fn player_input(
                 players.iter_mut(ecs).for_each(|(entity,pos)| {
                     let destination = *pos + delta;
                     commands.push(((), WantsToMove{ entity: *entity, destination}));
-                    did_something = true;
                 });
             }
         
             *turn_state = TurnState::PlayerTurn;
-        }
-
-        if !did_something {
-            if let Ok(mut health) = ecs.entry_mut(player_entity).unwrap().get_component_mut::<Health>() {
-                health.current = i32::min(health.max, health.current+1);
-            }
         }
     }
 }
